78c1793c27adadd4c0d36b6049e5a075da7501dd
Luciana B Ferrand <lbferrand@gmail.com>
2022-01-26 16:14:13 -0500

    Added alt element to image

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -4,7 +4,10 @@ export default function Phonetic(props) {
   return (
     <div className="Phonetic">
       <a href={props.phonetic.audio} target="_blank" rel="noreferrer">
-        <img src="https://img.icons8.com/stickers/30/000000/room-sound.png" />
+        <img
+          src="https://img.icons8.com/stickers/30/000000/room-sound.png"
+          alt="sound icon"
+        />
       </a>
       <br />
       {props.phonetic.text}
